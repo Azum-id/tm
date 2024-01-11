f58ed61bc13e03ffc4aab7c4466cc6d527a6afa4
Azum <cayna666id@gmail.com>
2024-01-12 02:59:45 +0700

    ?

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -104,13 +104,13 @@
             <!-- Content START -->
             <div class="content">
                 <div class="main">
-                    <div class="page-header">
+                    {{-- <div class="page-header">
                         <div class="breadcrumb">
                             <span class="me-1 text-gray"><i class="feather icon-home"></i></span>
                             <div class="breadcrumb-item"><a href="index.html"> Home </a></div>
                             <div class="breadcrumb-item"><a href="v-form-elements.html"> Tiket Masuk </a></div>
                         </div>
-                    </div>
+                    </div> --}}
                     @yield('content')
                 </div>
                 <!-- Footer START -->
