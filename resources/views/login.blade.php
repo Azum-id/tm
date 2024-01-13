eaefcd606ed441f1cb0232a03333e9806ddb63c9
Azum <cayna666id@gmail.com>
2024-01-13 18:47:47 +0700

    Errorrrrr!!!!!

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
     <title>Espire - Admin Dashboard Template</title>
 
     <!-- Favicon -->
